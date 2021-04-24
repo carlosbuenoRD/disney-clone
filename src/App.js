6e903f7e174e293edf148f34f103f4face66509c
Carlos Bueno <developer.carlosbueno@gmail.com>
2021-04-24 14:47:13 -0400

    Login component done!, starting header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/Header'
 import Login from './components/Login'
 
 function App() {
   return (
     <div className="app">
       <Router>
+        <Header />
         <Switch>
           <Route exact path='/' component={Login} />
         </Switch>
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,15 @@ const Login = () => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src='/images/cta-logo-one.svg' alt='brand logo'/>
+          <SignUp>Get All There</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+
+            subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src='/images/cta-logo-two.png' alt='brand logo'/>
+        </CTA>
         <BgImg />
       </Content>
     </Container>
@@ -41,6 +50,55 @@ const BgImg = styled.div`
   left: 0;
   right: 0;
   z-index: -1;
+`
+
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+  max-width: 600px;
+  min-height: 1px
+  display: block;
+  width: 100%;
+`
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  margin-top: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px; 
+  border: 1px solid transparent;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  width: 100%;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
 `
 
 export default Login
